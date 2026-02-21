1c476ca97f0ca9942e6b1e897d227c3d36bdc99d
brotherlogic <brotherlogic@gmail.com>
2026-02-21 15:39:46 -0800

    Supports filling tapes

--- postCreate.sh
+++ postCreate.sh
@@ -4,3 +4,4 @@ go install github.com/brotherlogic/recordgetter/recordgetter_cli@latest
 go install github.com/brotherlogic/recordcleaner/recordcleaner_cli@latest
 go install github.com/brotherlogic/recordfanout/recordfanout_cli@latest
 go install github.com/brotherlogic/gramophile/gram@latest
+go install github.com/brotherlogic/recordcollection/recordcollection_cli@latest
---
+++ tape.sh
@@ -0,0 +1,1 @@
+gram arrived $1 && recordcollection_cli budget $1 float2026 && recordcollection_cli is_tape $1 && recordcollection_cli spfolder $1 565206
