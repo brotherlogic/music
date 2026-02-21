81ebb67b99e562ae556ad6bd5724bbbb6b940f2e
brotherlogic <brotherlogic@gmail.com>
2026-02-21 23:21:34 +0000

    Adds post create

---
+++ clean.sh
@@ -0,0 +1,8 @@
+gram weight $1 $2 && gram width $1 $3 && gram sleeve $1 VinylStorageDoubleFlap && gram clean $1
+sleep 5
+recordfanout_cli ping --id $1
+recordfanout_cli ping --id $1
+recordfanout_cli ping --id $1
+recordfanout_cli ping --id $1
+recordfanout_cli ping --id $1
+recordfanout_cli ping --id $1
---
+++ postCreate.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+go install github.com/brotherlogic/recordgetter/recordgetter_cli@latest
+go install github.com/brotherlogic/recordcleaner/recordcleaner_cli@latest
