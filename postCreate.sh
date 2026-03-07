0405cc27e8705b689d8537239b56d97841391ca7
Simon Tucker <brotherlogic@gmail.com>
2026-03-06 18:00:52 -0800

    Fix container and post create to install tools

--- postCreate.sh
+++ postCreate.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+export GOPATH=/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+
 go install github.com/brotherlogic/recordgetter/recordgetter_cli@latest
 go install github.com/brotherlogic/recordcleaner/recordcleaner_cli@latest
 go install github.com/brotherlogic/recordfanout/recordfanout_cli@latest
